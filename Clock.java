0fed6345547fdef4bfc9778817d67490073f13e1
andyyoyo <andyyoyo76543@gmail.com>
2016-05-29 16:48:50 +0800

    set the buttons and their function

--- Clock.java
+++ Clock.java
@@ -12,6 +12,18 @@ public class Clock extends Application{
 	public void start(Stage primaryStage){
 		
 		ClockPane clock = new ClockPane();
+	
+		//make the buttons and  	
+		Button btStart = new Button("Start");
+		Button btStop = new Button("Stop");
+		btStart.setPrefSize(60,50);
+		btStop.setPrefSize(60,50);
+		
+		HBox h = new HBox(10);
+		BorderPane pane = new BorderPane();
+		h.getChildren().addAll(btStart, btStop);
+		pane.setCenter(clock);
+		pane.setBottom(h);		
 		
 		EventHandler<ActionEvent> eventHandler = e->{
 			clock.setCurrentTime();
@@ -21,7 +33,19 @@ public class Clock extends Application{
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.play();
 		
-		Scene scene = new Scene(clock,250,280);
+		//Set button action
+		btStart.setOnAction(e -> {
+			//Play animation
+			animation.play();
+		});
+		
+		//Set button action
+		btStop.setOnAction(e -> {
+			//Stop animation
+			animation.stop();
+		});		
+		
+		Scene scene = new Scene(pane,250,280);
 		primaryStage.setTitle("ClockAnimation");
 		primaryStage.setScene(scene);
 		primaryStage.show();
